d8c32a350185ef168915ef1b7f81e91cce15743c
yuehongzhang001 <51759777+yuehongzhang001@users.noreply.github.com>
2021-09-26 09:16:33 -0500

    Update waiting-list.vue

--- src/views/waiting-list.vue
+++ src/views/waiting-list.vue
@@ -472,7 +472,7 @@
 
                           <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
                           <!-- <script
-                            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBFrzy71_H7XTAZqAhSQf2yjPNUsKNYldk&callback=initMap&libraries=&v=weekly"
+                            src="https://maps.googleapis.com/maps/api/js?key=****"
                             async
                           ></script> -->
                         </div>
